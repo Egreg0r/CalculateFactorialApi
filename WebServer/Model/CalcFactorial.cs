a923b3f8fd93c3115c53fb60f6b143eaa337e78f
Egreg0r <maximmus2510@gmail.com>
2021-10-10 22:46:35 +1000

    1. WebServer Model.CalcFactorial     В Добавлены Описания имен

--- WebServer/Model/CalcFactorial.cs
+++ WebServer/Model/CalcFactorial.cs
@@ -1,11 +1,15 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebServer.Models
 {
     public class CalcFactorial
     {
+        [DisplayName("Исходное число")]
         public long UserNomber { get; set; }
 
+        [DisplayName("Факториал числа")]
         public string Factorial { get; set; }
     }
 }
